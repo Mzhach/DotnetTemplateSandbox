e7a1df78b1f5e8ea225c02dceff34f76848ef9f3
Илья Мжачев <i.mzhachev@yandex.ru>
2023-09-22 23:40:32 +0300

    Add service skeleton

---
+++ src/ServiceTemplate.Api/Controllers/SampleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ServiceTemplate.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class SampleController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Ping()
+    {
+        return Ok("Pong");
+    }
+}
---
+++ tests/ServiceTemplate.Application.UnitTests/UnitTest.cs
@@ -0,0 +1,11 @@
+﻿using Xunit;
+
+namespace ServiceTemplate.Application.UnitTests;
+
+public class UnitTest
+{
+    [Fact]
+    public void Test()
+    {
+    }
+}
